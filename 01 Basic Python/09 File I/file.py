d0334aa6abade93eaf4d63bb230710b76a596947
Radhika Parmar <radhikaparmar@Radhis-Mac-Air.local>
2025-05-24 20:42:34 +0530

    minor changes

--- 01 Basic Python/09 File I/file.py
+++ 01 Basic Python/09 File I/file.py
@@ -6,3 +6,15 @@ f.close()
 f = open('myfile.txt','w')
 f.write("This is another sample file.")
 f.close()
+
+f = open("myfile.txt")
+print(f.readline())
+f.close()
+
+f = open("file.txt", 'rt')
+print(f.readline(2))
+f.close()
+
+f = open("myfile.txt")
+print(f.readlines())
+f.close()
---
+++ Python Libraries/numpy_lib.py
@@ -0,0 +1,8 @@
+# Numpy is the python library for scientific computing in pyton
+
+# Advatages: less memory, fast and convinient
+
+import numpy as np
+
+a = np.array([(1, 2, 3), (4, 5, 6)])
+print(f"Two dimensional array is: {a}")
